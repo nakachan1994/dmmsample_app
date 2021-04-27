26025bb79047f709bdfd6c8c4267284271f95729
nakachan1994 <nakachaaaan@gmail.com>
2021-04-27 01:27:52 +0000

    [add]ハンバーガーメニュー

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  get 'homes/about', to:'homes#about', as:'about_homes'
   resources :todolists do
     resource :completes, only:[:create, :destroy]
   end
